4567226e0a5014dce84c594928bc586dcb3a0a28
RezaAstarki <reza.astaraky@gmail.com>
2024-07-17 23:39:20 +0330

    fix home bug

--- src/app/(static)/page.tsx
+++ src/app/(static)/page.tsx
@@ -2,17 +2,7 @@
 import { AtomIcon, Edit, Share2 } from "lucide-react";
 import Loading from "../(dynamic)/resume/[resume_id]/view/loading";
 import BoxSpinner from "@/components/custom components/loadings/BoxSpinner";
-import { Button } from "@/components/ui/button";
-import {
-  generateSummeryFromAI,
-  getAiResponse,
-} from "@/service/strapiCms/serverActions";
 
 export default function HomePage() {
-  return (
-    <main className="flex flex-col flex-1 p-5 md:p-14">
-      <Button>ai</Button>
-      home
-    </main>
-  );
+  return <main className="flex flex-col flex-1 p-5 md:p-14">home</main>;
 }
